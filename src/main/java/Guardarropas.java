8f66fdc07110ba3ef4d4d0067b2e6fee8dc39d3c
jaks97 <jkhalilsilva@gmail.com>
2020-04-13 00:35:29 -0300

    Elimino clase guardarropas que no tiene comportamiento

--- src/main/java/Guardarropas.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public class Guardarropas {
-    private List<Prenda> prendas;
-
-    public Guardarropas(List<Prenda> prendas) {
-        this.prendas = prendas;
-    }
-}
